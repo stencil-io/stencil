17c93509c233dc98732b06228fee63f125cbc97d
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2023-04-21 15:06:45 +0300

    auth serialization

--- quarkus-extensions/quarkus-user-actions-parent/deployment/src/main/java/io/thestencil/quarkus/useractions/deployment/UserActionsProcessor.java
+++ quarkus-extensions/quarkus-user-actions-parent/deployment/src/main/java/io/thestencil/quarkus/useractions/deployment/UserActionsProcessor.java
@@ -41,6 +41,7 @@ import io.quarkus.vertx.http.deployment.devmode.NotFoundPageDisplayableEndpointB
 import io.thestencil.iam.api.ImmutableAttachment;
 import io.thestencil.iam.api.ImmutableAttachmentData;
 import io.thestencil.iam.api.ImmutableAttachmentDownloadUrl;
+import io.thestencil.iam.api.ImmutableAuthorizationAction;
 import io.thestencil.iam.api.ImmutableUserAction;
 import io.thestencil.iam.api.ImmutableUserMessage;
 import io.thestencil.iam.spi.integrations.ImmutableProcessesInit;
@@ -138,6 +139,7 @@ public class UserActionsProcessor {
     List<String> names = new ArrayList<>();
     
     for(Class<?> clazz : Arrays.asList(
+        ImmutableAuthorizationAction.class,
         ImmutableAttachmentDownloadUrl.class,
         ImmutableUserAction.class,
         ImmutableAttachment.class,
