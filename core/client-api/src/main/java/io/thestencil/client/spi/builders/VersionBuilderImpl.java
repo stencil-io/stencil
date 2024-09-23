45d0ede312ac94e4b2af201534af8ecbd46fefa1
jocelyn.mutso <jocelyn.mutso@gmail.com>
2024-09-23 18:26:17 +0000

    Release: 1.148.40

--- core/client-api/src/main/java/io/thestencil/client/spi/builders/VersionBuilderImpl.java
+++ core/client-api/src/main/java/io/thestencil/client/spi/builders/VersionBuilderImpl.java
@@ -28,7 +28,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class VersionBuilderImpl implements VersionBuilder {
 
-  private static final String VERSION = "1.148.39";
+  private static final String VERSION = "1.148.40";
   private static final String DATE = "23/09/2024";
 
   @Override
